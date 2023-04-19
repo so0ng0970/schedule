5e54a2a1cc7d6f4d95d5348802e023660f5215cb
soonger3306 <soonger3306@daum.net>
2023-04-19 22:53:50 +0900

    database

---
+++ lib/database/drift_database.dart
@@ -0,0 +1,31 @@
+// private 값들은 불러올 수 없다.
+import 'dart:io';
+import 'package:drift/drift.dart';
+import 'package:drift/native.dart';
+import 'package:schedule/model/category_color.dart';
+import 'package:schedule/model/schedule.dart';
+import 'package:path/path.dart' as p;
+import 'package:path_provider/path_provider.dart';
+// private 값까지 불러 올 수 있다.
+part 'drift_database.g.dart';
+
+@DriftDatabase(
+  tables: [
+    Schedules,
+    CategoryColors,
+  ],
+)
+//  _$LocalDatabase 'drift_database.g.dart' 생성되면서 drift가 만들어줌
+class LocalDatabase extends _$LocalDatabase {
+  LocalDatabase() : super(_openConnection());
+}
+// 어떤 위치에 지정한 파일로 데이터 베이스 만듬
+LazyDatabase _openConnection() {
+  return LazyDatabase(() async {
+    final dbFolder = await getApplicationDocumentsDirectory();
+    final file = File(
+      p.join(dbFolder.path, 'db.sqlite'),
+    );
+    return NativeDatabase(file);
+  });
+}
--- lib/model/category_color.dart
+++ lib/model/category_color.dart
@@ -1,5 +1,6 @@
 import 'package:drift/drift.dart';
 
+
 class CategoryColors extends Table {
   // PRIMARY KEY
   IntColumn get id => integer().autoIncrement()();
