13f35e0ab1653fd258c9953acd4cad6cb164a9f3
soonger3306 <soonger3306@daum.net>
2023-04-21 20:49:38 +0900

    database

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,45 @@
+import 'package:drift/drift.dart';
 import 'package:flutter/material.dart';
 import 'package:intl/date_symbol_data_local.dart';
+import 'package:schedule/database/drift_database.dart';
 import 'package:schedule/screen/home_screen.dart';
+
+const DEFAULT_COLORS = [
+  // 빨강
+  'F44336',
+  // 주황
+  'FF9800'
+      // 노랑
+      'FFEB3B'
+      // 초록
+      'FCAF50'
+      // 파랑
+      '2196F3'
+      // 남
+      '3F51B5'
+      // 보라
+      '9C27B0'
+];
 
 void main() async {
   // 플러터 프레임 워크가 준비되는지 확인 해줘야 한다.
   WidgetsFlutterBinding.ensureInitialized();
-  
+
   await initializeDateFormatting();
 
+  final database = LocalDatabase();
+
+  final colors = await database.getCategoryColors();
+  if (colors.isEmpty) {
+    for (String hexCode in DEFAULT_COLORS) {
+      await database.createCategoryColors(
+        CategoryColorsCompanion(
+          hexCode: Value(hexCode),
+        ),
+      );
+    }
+  }
+  print(await database.getCategoryColors());
   runApp(const MyApp());
 }
 
